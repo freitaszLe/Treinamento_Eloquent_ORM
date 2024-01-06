76a1dc386eda2aa66316c6d5f3825edf2e945ceb
Elton Fonseca <elton869@gmail.com>
2024-01-06 12:14:55 -0300

    17 - Mostrando os dados usando uma tabela

--- resources/views/clientes/index.blade.php
+++ resources/views/clientes/index.blade.php
@@ -1,4 +1,60 @@
-@foreach($clientes as $cliente)
-    {{ $cliente->nome }} | {{ $cliente->endereco }} | {{ $cliente->descricao }}
-    </br>
-@endforeach
+<!doctype html>
+<html>
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <script src="https://cdn.tailwindcss.com"></script>
+</head>
+<body>
+    <nav class="bg-gray-300">
+        <div class="container mx-auto flex items-center justify-between p-4">
+            <a href="" class="text-2xl font-semibold">Treinaweb</a>
+        
+            <ul class="font-medium flex">
+                <li class="px-4">Cadastro de Clientes</li>
+            </ul>
+        </div>
+    </nav>
+
+    <div class="container mx-auto">
+        <h1 class="text-4xl font-bold text-center my-4">
+            Lista de Clientes
+        </h1>
+
+        <div class="relative overflow-x-auto">
+            <table class="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
+                <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+                    <tr>
+                        <th scope="col" class="px-6 py-3">
+                            Nome
+                        </th>
+                        <th scope="col" class="px-6 py-3">
+                            Endereço
+                        </th>
+                        <th scope="col" class="px-6 py-3">
+                            Descrição
+                        </th>
+                    </tr>
+                </thead>
+                <tbody>
+                    @foreach($clientes as $cliente)
+                        <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+                            <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                                {{ $cliente->nome }}
+                            </th>
+                            <td class="px-6 py-4">
+                                {{ $cliente->endereco }}
+                            </td>
+                            <td class="px-6 py-4">
+                                {{ $cliente->descricao }}
+                            </td>
+                        </tr>
+                    @endforeach
+                </tbody>
+            </table>
+        </div>
+    </div>
+</body>
+</html>
+
+
